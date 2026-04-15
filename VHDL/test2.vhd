ecb3bca0d5db2913cef52c8d01a423cd28f93a51
Klaus Grozav <gklau@tutamail.com>
2026-04-15 12:54:38 +0300

    Commit on Lesson 11

---
+++ VHDL/test2.vhd
@@ -0,0 +1,15 @@
+entity test2 is
+
+
+end entity;
+
+
+architecture sim of test2 is
+begin
+
+	process is
+	begin
+		report " this is my other commit ";
+		wait for 10 ns;
+	end process;
+end architecture;
